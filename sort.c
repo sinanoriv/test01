cef034977b141a1aea2d5823acf1ab3424bce2db
Shinano <shinano@ShinanonoMacBook-Pro.local>
2023-02-18 19:08:18 +0900

    最大値を算出する処理を追加

--- sort.c
+++ sort.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#define NUMBER 5
 int main(void) {
-  int sortedTargetArray[5] = {1,3,5,6,8};
-  for (int i = 0; i < 5; i++) {
-    printf("%d\n", sortedTargetArray[i]);
+  int targetArray[NUMBER] = {1, 3, 5, 6, 8};
+  int max = 0;
+  
+  for (int i = 0; i < NUMBER; i++) {
+    if (max < targetArray[i]) {
+      max = targetArray[i];
+    }     
   }
   return 0;
 }
+
