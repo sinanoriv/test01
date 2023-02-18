698af6d42e2e3843a9d71d7c9d1b5dfe8b938c48
Shinano <shinano@ShinanonoMacBook-Pro.local>
2023-02-18 23:18:19 +0900

    配列でソートを行うよう改善しました

--- sort.c
+++ sort.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
+#include <limits.h>
 
 #define NUMBER 5
 
-int main(void) {
-  int targetArray[NUMBER] = {1, 3, 5, 6, 8};
+int main(void)
+{
+  int targetArray[NUMBER] = {3, 3, 45, 6, 8};
+  int resultArray[NUMBER];
   int max = 0;
-  for (int i = 0; i < NUMBER; i++) {
-    if (max < targetArray[i]) {
-      max = targetArray[i];
+
+  for (int j = 0; j < NUMBER; j++)
+  {
+    for (int i = 0; i < NUMBER; i++)
+    {
+      if (max <= targetArray[i])
+      {
+        max = targetArray[i];
+      }
     }
+    resultArray[j] = max;
+
+    for (int i = 0; i < NUMBER; i++)
+    {
+      if (targetArray[i] == max)
+      {
+        targetArray[i] = INT_MIN;
+        break;
+      }
+    }
+    max = INT_MIN;
   }
-  int max2 = 0 ;
-  for (int i = 0; i < NUMBER - 1; i++) {
-    if (max2 < targetArray[i]) {
-      max2 = targetArray[i];
-    }
+
+  for (int j = 0; j < NUMBER; j++)
+  {
+    printf("%d,", resultArray[j]);
   }
-  int max3 = 0 ;
-  for (int i = 0; i < NUMBER - 2; i++) {
-    if (max3 < targetArray[i]) {
-      max3 = targetArray[i];
-    }
-  }int max4 = 0 ;
-  for (int i = 0; i < NUMBER - 3; i++) {
-    if (max4 < targetArray[i]) {
-      max4 = targetArray[i];
-    }
-  }int max5 = 0 ;
-  for (int i = 0; i < NUMBER - 4; i++) {
-    if (max5 < targetArray[i]) {
-      max5 = targetArray[i];
-    }
-  }
-  printf("%d,%d,%d,%d,%d" , max, max2, max3 ,max4 ,max5);
-  
   return 0;
 }
